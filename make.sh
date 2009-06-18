5efdc7998b10acff48dbd4975ae321022e78200c
Olivier Cochard-Labbé <olivier@cochard.me>
2009-06-18 19:53:10 +0000

    Remove FlashDevice: use the file include in NanoBSD because there will be no change in this file

--- make.sh
+++ make.sh
@@ -1,20 +1,26 @@
-#!/bin/sh
+#!/bin/tcsh
 # Make script for BSD Router Project
 
 # Uncomment for enable debug: 
 #set -x
 
-NANOBSD_DIR=/usr/src/tools/tools/nanobsd
+set NANOBSD_DIR=/usr/src/tools/tools/nanobsd
 
-#Flash database to copy
+#Compact flash database needed for NanoBSD
+cp $NANOBSD_DIR/FlashDevice.sub .
+
 #TO DO: get actual pwd, and use it for NANOBSD variables
 
 #TO DO: Check that we are under FreeBSD current
 
+
+set LOCAL_ARCH=`uname -p`
+echo "test arch:" $LOCAL_ARCH
+
 echo "BSDRP make script"
 echo ""
 echo "Enter architecture (i386/amd64): "
-while INPUT_ARCH="i386" || "amd64"
+while ( "$INPUT_ARCH == "i386" )
 do
 	read INPUT_ARCH <&1
 done
@@ -31,3 +37,4 @@ echo "$line $word"
 
 #sh $NANOBSD_DIR/nanobsd.sh -b -c BSDRP.nano
 #sh ../nanobsd.sh -c BSDRP.nano
+exit 0
