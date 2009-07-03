41b33eb04414318966212ddade994182cb2d0c1b
cochard <cochard@d46d7a13-abdd-4655-8a92-949674c7dda5>
2009-07-03 21:53:38 +0000

    fix bugs in scripts and adapt config tool to data partition

--- trunk/make.sh
+++ trunk/make.sh
@@ -34,6 +34,9 @@
 # Uncomment for enable debug: 
 #set -x
 
+# Exit if error
+#set -e
+
 NANOBSD_DIR=/usr/src/tools/tools/nanobsd
 
 #Compact flash database needed for NanoBSD ?
@@ -144,7 +147,7 @@ pprint 1 "Console type for boot message:"
 pprint 1 " - vga : vga only"
 pprint 1 " - dual : vga and serial (BUGGY and serial port mandatory!)"
 pprint 1 " - serial : serial only"
-while [ "$INPUT_CONSOLE" != "vga" -a "$INPUT_CONSOLE" != "serial" -a ]"$INPUT_CONSOLE" != "dual" 
+while [ "$INPUT_CONSOLE" != "vga" -a "$INPUT_CONSOLE" != "serial" -a "$INPUT_CONSOLE" != "dual" ]
 do
 	read INPUT_CONSOLE <&1
 done
