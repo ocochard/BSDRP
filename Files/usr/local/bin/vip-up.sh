6c8a6da6404c55bd389f06e6fc1416b0bee15e39
Olivier Cochard-Labbé <olivier@cochard.me>
2010-07-27 10:22:49 +0000

    ucarp: use allready existing ucarp-up and ucarp-down script, fix blanktimevidcontrol error message (thanks to Nugroho Atmotaruno), merge diff on the existing /boot/loader.conf during upgrade

--- Files/usr/local/bin/vip-up.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-exec 2> /dev/null
-logger ucarp vip-up script called: Adding $2 as alias on $1
-if ! /sbin/ifconfig "$1" alias "$2" netmask 255.255.255.255; then
-	logger ucarp vip-up script failled to configure alias $2 on $1!
-fi
