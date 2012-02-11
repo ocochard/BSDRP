393f45af47aef97b675d7ff7a428979e25ceed7b
cochard <cochard@d46d7a13-abdd-4655-8a92-949674c7dda5>
2012-02-11 00:07:43 +0000

    Add kbdcontrol and syscons keyboard files

--- trunk/make.sh
+++ trunk/make.sh
@@ -100,7 +100,7 @@ update_src () {
 *default compress
 
 src-all tag=RELENG_8_2
-ports-all date=2012.02.05.00.00.00
+ports-all date=2012.02.10.00.00.00
 EOF
 	csup -L 1 $SUPFILE
     # Force a repatch because csup pulls pristine sources.
