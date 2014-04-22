78ffb527baca2fd0ec7e8859e968b9fb998fa3c8
Olivier Cochard-Labbé <olivier@cochard.me>
2014-04-22 19:28:15 +0000

    update source tree, including merging nanobsd.sh update

--- nanobsd.sh
+++ nanobsd.sh
@@ -188,9 +188,9 @@ nano_cleanup ( ) (
 clean_build ( ) (
 	pprint 2 "Clean and create object directory (${MAKEOBJDIRPREFIX})"
 
-	if ! rm -rf ${MAKEOBJDIRPREFIX} > /dev/null 2>&1 ; then
-		chflags -R noschg ${MAKEOBJDIRPREFIX}
-		rm -r ${MAKEOBJDIRPREFIX}
+	if ! rm -xrf ${MAKEOBJDIRPREFIX}/ > /dev/null 2>&1 ; then
+		chflags -R noschg ${MAKEOBJDIRPREFIX}/
+		rm -xr ${MAKEOBJDIRPREFIX}/
 	fi
 	mkdir -p ${MAKEOBJDIRPREFIX}
 	printenv > ${MAKEOBJDIRPREFIX}/_.env
@@ -243,17 +243,17 @@ build_kernel ( ) (
 clean_world ( ) (
 	if [ "${NANO_OBJ}" != "${MAKEOBJDIRPREFIX}" ]; then
 		pprint 2 "Clean and create object directory (${NANO_OBJ})"
-		if ! rm -rf ${NANO_OBJ} > /dev/null 2>&1 ; then
+		if ! rm -rxf ${NANO_OBJ}/ > /dev/null 2>&1 ; then
 			chflags -R noschg ${NANO_OBJ}
-			rm -r ${NANO_OBJ}
+			rm -xr ${NANO_OBJ}/
 		fi
 		mkdir -p ${NANO_OBJ} ${NANO_WORLDDIR}
 		printenv > ${NANO_OBJ}/_.env
 	else
 		pprint 2 "Clean and create world directory (${NANO_WORLDDIR})"
-		if ! rm -rf ${NANO_WORLDDIR}/ > /dev/null 2>&1 ; then
+		if ! rm -rxf ${NANO_WORLDDIR}/ > /dev/null 2>&1 ; then
 			chflags -R noschg ${NANO_WORLDDIR}
-			rm -rf ${NANO_WORLDDIR}
+			rm -rxf ${NANO_WORLDDIR}/
 		fi
 		mkdir -p ${NANO_WORLDDIR}
 	fi
@@ -1075,7 +1075,7 @@ cust_pkg () (
 			exit 2
 		fi
 	done
-	rm -rf ${NANO_WORLDDIR}/Pkg
+	rm -rxf ${NANO_WORLDDIR}/Pkg
 )
 
 cust_pkgng () (
@@ -1144,7 +1144,7 @@ cust_pkgng () (
 			exit 2
 		fi
 	done
-	rm -rf ${NANO_WORLDDIR}/Pkg
+	rm -rxf ${NANO_WORLDDIR}/Pkg
 )
 
 #######################################################################
