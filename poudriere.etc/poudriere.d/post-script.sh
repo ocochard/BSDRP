d06cf90e81d526e7118ec6f29456c07dc550ffdd
Olivier Cochard-Labbé <olivier@cochard.me>
2023-10-10 01:22:47 +0200

    Milestone: poudriere image is now installing on a 2G media size :-)

--- poudriere.etc/poudriere.d/post-script.sh
+++ poudriere.etc/poudriere.d/post-script.sh
@@ -1,27 +1,14 @@
 #!/bin/sh
 #
-# Not all WITHOUT_ options are correctly use during image generation
+# Not all WITHOUT_ options are correctly applied during image generation
 
 # XXX Need to build /usr/src/tools/tools/netrate/netblast&netreceive
 
-# XXX Need to extract debug symbols first
+# If port related, it is recommanded to add list of file in the pkg.conf file
+# That will avoid installing files during packages installation
 TO_REMOVE='
-usr/sbin/config
-usr/lib/debug
-usr/sbin/bsdconfig
-usr/share/bsdconfig
-usr/share/bhyve
-usr/share/examples
-usr/share/flua
-usr/share/doc
-usr/share/man/man3
-usr/share/man/man3lua
-usr/share/man/man9
-usr/share/mk
-usr/share/openssl
+/usr/local/sbin/pkg-static
 usr/include
-usr/share/misc/magic.mgc
-usr/share/misc/termcap.db
 usr/local/include
 '
 
@@ -30,11 +17,8 @@ if [ -z "${WORLDDIR}" ]; then
 	exit 1
 fi
 
-#for i in ${TO_REMOVE}; do
-#	if [ -e ${WORLDDIR}/$i ]; then
-#		rm -rf ${WORLDDIR}/$i
-#	fi
-#done
-
-# Kill all .a's that are installed with TOOLCHAIN (remove 33MB)
-#find ${WORLDDIR} -type f -name \*.a | xargs rm
+for i in ${TO_REMOVE}; do
+	if [ -e ${WORLDDIR}/$i ]; then
+		rm -rf ${WORLDDIR}/$i
+	fi
+done
