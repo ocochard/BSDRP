1880006b373131ac5a89778f84b77c56488e881a
Olivier Cochard-Labbé <olivier@cochard.me>
2025-09-28 07:56:31 +0200

    cleanup the release script

--- tools/release.sh
+++ tools/release.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-#This script permit to help the generation and uploading process of new images
+# This script upload images and generate dokuwiki
 # XXX sourceforge downloading speed too slow need to enable mirrors
-#It generate the dokuwiki table too
 # For list of installed software to be include in CHANGES
 # /usr/local/poudriere/data/images/packages.list
 # For list of installed software to be incluled in AUTHORS
@@ -12,96 +11,22 @@ set -eu
 die() { echo -n "EXIT: " >&2; echo "$@" >&2; exit 1; }
 
 # General variables
-#DRY="echo"
-# Set PROJECT variable like this example:
-# env PROJECT=BSDRPcur tools/release.sh generate
-# env DISK_SIZE=512 tools/release.sh generate fast
-DRY=""
-SRC_DIR="/usr/local/BSDRP"
-: ${PROJECT:=BSDRP}
-OBJ_BASE_DIR="$(pwd)"/workdir
-VERSION=""
-FAST_MODE=false
-if [ $(uname -m) = "sparc64" ]; then
-	ARCH_LIST="sparc64"
-else
-	ARCH_LIST='
-amd64
-'
-fi
-CONSOLE_LIST='
-vga
-serial
-'
+: ${DRY:=""}
+poudriere_imgdir="/usr/local/poudriere/data/images"
 
 usage () {
-	echo "$0 [ generate | upload | dokuwiki ]"
-	echo " - generate [fast]: clean and generate all arch/console images"
-	echo "                    fast mode avoid rebuilding all"
+	echo "$0 [ upload | dokuwiki ]"
 	echo " - upload: Upload all images to SourceForge"
 	echo " - dokuwiki: Generate dokuwiki table of images"
 	exit 0
 }
 
-##### Check if previous NanoBSD make stop correctly by unoumt all tmp mount
-# exit with 0 if no problem detected
-# exit with 1 if problem detected, but clean it
-# exit with 2 if problem detected and can't clean it
-check_clean() {
-    # Patch from Warner Losh (imp@)
-	__a=$(mount | grep $1 | awk '{print length($3), $3;}' | sort -rn | awk '{$1=""; print;}')
-	if [ -n "$__a" ]; then
-        echo "unmounting $__a"
-		umount $__a
-    fi
-}
-
-generate(){
-	[ -d ${SRC_DIR} ] || die "Doesn't found source dir: ${SRC_DIR}"
-
-	# Build of each arch/console
-    for arch in ${ARCH_LIST}; do
-		# Initial build (update and rebuild all)
-		# This build can let unclean building dependencies
-		if ! ($FAST_MODE); then
-			( cd ${SRC_DIR}
-        	${DRY} ./make.sh -C -p ${PROJECT} -u -f -y
-        	)
-		fi
-		if ! ($FAST_MODE); then
-        	[ -f ${OBJ_BASE_DIR}/${PROJECT}.${arch}/_.mtree ] || die "problem during initial build of ${arch}"
-		fi
-		for console in ${CONSOLE_LIST}; do
-		# Definitive build, only installing pkg generated previously
-			[ "${arch}" = "i386_xenpv" -a "${console}" = "serial" ] && continue
-			[ "${arch}" = "sparc64" -a "${console}" = "serial" ] && continue
-			[ ! -f ${PROJECT}/kernels/${arch} ] && continue
-        	( cd ${SRC_DIR}
-        	${DRY} ./make.sh -p ${PROJECT} -b -c ${console}
-        	)
-			if [ "${arch}" = "i386_xenpv" -o "${arch}" = "sparc64" ]; then
-   				[ -f ${OBJ_BASE_DIR}/${PROJECT}.${arch}/BSDRP-${VERSION}-${arch}.mtree.xz ] || die "problem during final build regarding of ${arch}-${console}"
-	     	else
-				[ -f ${OBJ_BASE_DIR}/${PROJECT}.${arch}/BSDRP-${VERSION}-${arch}-${console}.mtree.xz ] || die "problem during final build regarding of ${arch}-${console}"
-			fi
-			echo "done" > ${OBJ_BASE_DIR}/${PROJECT}.${arch}/release.done
-		done
-    done
-	exit 0
-}
-
 upload(){
-	${DRY} scp ${PROJECT}/CHANGES.md cochard,bsdrp@frs.sourceforge.net:/home/frs/project/b/bs/bsdrp/BSD_Router_Project/$1
-	FILE_LIST=''
-	if [ -d ${OBJ_BASE_DIR}/${PROJECT}.sparc64 ]; then
-		ARCH_LIST="${ARCH_LIST}sparc64"
-	fi
-	for arch in ${ARCH_LIST}; do
-		if [ -f ${OBJ_BASE_DIR}/${PROJECT}.${arch}/release.done ]; then
-			FILE_LIST=$(ls ${OBJ_BASE_DIR}/${PROJECT}.${arch}/BSDRP-*)
-			${DRY} scp ${FILE_LIST} cochard,bsdrp@frs.sourceforge.net:/home/frs/project/b/bs/bsdrp/BSD_Router_Project/$1/${arch}
-		fi
-	done
+  local ver=$1
+  local arch=$2
+	${DRY} scp CHANGES.md cochard,bsdrp@frs.sourceforge.net:/home/frs/project/b/bs/bsdrp/BSD_Router_Project/${ver}
+  FILE_LIST=$(ls ${poudriere_imgdir}/BSDRP-${ver}-*-${arch}.*)
+			${DRY} scp ${FILE_LIST} cochard,bsdrp@frs.sourceforge.net:/home/frs/project/b/bs/bsdrp/BSD_Router_Project/${ver}/${arch}
 	exit 0
 }
 
@@ -176,33 +101,29 @@ if [ $# -lt 1 ]; then
 	usage
 fi
 
+if [ -r BSDRP/Files/etc/version ]; then
+  version=$(cat BSDRP/Files/etc/version)
+else
+  die "No BSDRP/Files/etc/version"
+fi
+
+arch=$(uname -p)
+
 if [ "$1" = "upload" -a $# -eq 1 ]; then
-    echo "Missing destination directory (like: nightly/2012-09-05)"
-    echo
-	usage
+  echo "Missing destination directory, examples:"
+  echo "- nightly/2012-09-05"
+  echo "- 2.0"
+  echo
+  usage
 fi
 
 
 if [ "$1" = "dokuwiki" -a $# -eq 1 ]; then
-    echo "Missing destination directory (like: nightly/2012-09-05)"
-    echo
-	usage
+  echo "Missing destination directory, examples:"
+  echo "- nightly/2012-09-05"
+  echo "- 2.0"
+  echo
+  usage
 fi
 
-if [ ! -d ${SRC_DIR} ]; then
-	echo "You need to install BSDRP source"
-else
-    if [ -f ${SRC_DIR}/${PROJECT}/Files/etc/version ]; then
-		VERSION=$(cat ${SRC_DIR}/${PROJECT}/Files/etc/version)
-	else
-		MASTER=$(grep -E '^MASTER_PROJECT=' ${PROJECT}/make.conf | cut -d '"' -f 2)
-		VERSION=$(cat ${SRC_DIR}/${MASTER}/Files/etc/version)
-	fi
-fi
-
-if [ $# -eq 1 ]; then
-	$1
-elif [ $2 = "fast" ]; then
-	FAST_MODE=true
-fi
-$1 $2
+$1 ${version} ${arch}
